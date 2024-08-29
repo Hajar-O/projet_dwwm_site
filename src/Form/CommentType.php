d317f690655da68b012a72e9c2736858b9d84f36
Hajar-O <hajar.ouhadi@lapiscine.pro>
2024-08-29 23:24:23 +0200

    CSS_section_textarea_reviews

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -7,6 +7,7 @@ use App\Entity\Recipe;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,13 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('text')
+            ->add('text', TextareaType::class, [
+                'label' => 'Commentaire',
+                'attr' => [
+                    'class' => 'reviewArea',
+                    'placeholder' => 'Mon commentaire...'
+                ]
+            ])
             /*->add('publishedAt', null, [
                 'widget' => 'single_text',
             ])
