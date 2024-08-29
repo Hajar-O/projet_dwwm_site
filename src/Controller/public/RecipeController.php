4f76fae9dff80814715373c40873ac9386fda527
Hajar-O <hajar.ouhadi@lapiscine.pro>
2024-08-29 18:55:14 +0200

    form_reviews_recipe

--- src/Controller/public/RecipeController.php
+++ src/Controller/public/RecipeController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Controller\public;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\CategoryRepository;
+use App\Repository\CommentRepository;
 use App\Repository\RecipeIngredientRepository;
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -58,13 +63,47 @@ class RecipeController extends AbstractController
     }
 
     #[Route('/recipe/all/{id}', name: 'recipe')]
-    public function showRecipe(int $id,RecipeIngredientRepository $ingredientRepository, RecipeRepository $recipeRepository)
+    public function showRecipe(int $id,RecipeIngredientRepository $ingredientRepository,CommentRepository $commentRepository, RecipeRepository $recipeRepository, Request $request, EntityManagerInterface $entityManager)
     {
         $recipe = $recipeRepository->find($id);
         $ingredients = $ingredientRepository->findAll();
+
+        if (!$recipe) {
+            $html404 = $this->renderView('public/error/404.html.twig');
+            return new Response($html404, 404);
+        }
+
+        //je récupère l'user connecté
+
+        $user = $this->getUser();
+
+        $review = new Comment;
+        $review->setIdRecipe($recipe);
+        $review->setIdUser($user);
+        $review->setPublishedAt(new \DateTimeImmutable('now'));
+
+        //je créé un formulaire de commentaire
+
+        $reviewsForm = $this->createForm(CommentType::class, $review);
+        $reviewsForm->handleRequest($request);
+
+        if($reviewsForm->isSubmitted() && $reviewsForm->isValid()){
+            $entityManager->persist($review);
+            $entityManager->flush();
+
+            //redirection pour éviter la redirection
+            return $this->redirectToRoute('recipe_show', ['id' => $recipe->getId()]);
+        }
+
+        $reviews = $commentRepository->findBy(['idRecipe'=> $recipe]);
+
         return $this->render('public/recipe/recipe-show.html.twig', [
             'recipe' => $recipe,
-            'ingredients' => $recipe->getRecipeIngredients()
+            'ingredients' => $recipe->getRecipeIngredients(),
+            'reviews' => $reviews,
+            'reviewsForm'=> $reviewsForm
         ]);
     }
+
+
 }
